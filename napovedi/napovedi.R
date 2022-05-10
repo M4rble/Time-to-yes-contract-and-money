ce6789aa1c51e8aebb2b0f48b76c7b037b4822a0
M4rble <anze.mramor@student.fmf.uni-lj.si>
2022-05-10 13:33:14 +0200

    delitev v train in test set po 2 metodah

--- napovedi/napovedi.R
+++ napovedi/napovedi.R
@@ -4,7 +4,7 @@
 # - ustvarjanje novih atributov
 # - PAZI NA ID! - vsi z istim ID so 1 podatek - dodaj nove stolpce kot recimo število kreditov (in koliko katerih), 0 in 1
 # - učno in testno množico je treba ustrezno razdeliti - VSI z istim ID v eno ali drugo!!
-# - KNN, random forests, lin regression, decision trees? Probaj vse
+# - KNN, random forests, lin regression, decision trees, lmer? Probaj vse
 
 
 # za osamelce - SVM (metoda podpornih vektorjev - za odkrivanje osamelcev)
@@ -58,6 +58,10 @@ corrplot(cor_TTY, type = "upper", order = "hclust",
          tl.col = "black", tl.srt = 45)
 
 
+
+# 1. PRISTOP - krediti znotraj istega id-ja so neodvisni
+#=======================================================
+
 k <- 10
 r <- unique(sample(podatki.TTY$ID))
 # razrežemo na k intervalov
@@ -78,19 +82,19 @@ test.data$ID %in% train.data$ID
 
 
 
-napaka.cv <- function(podatki, formula, k){
+napaka.cv <- function(podatki_vis, podatki_id, formula, k){
   set.seed(42)
   # za k-kratno prečno preverjanje najprej podatke razdelimo na k enako velikih delov
 
   # najprej naključno premešamo id-je
-  r <- unique(sample(podatki.TTY$ID))
+  r <- unique(sample(podatki_id))
   # razrežemo na k intervalov
   razrez <- cut(seq_along(r), k, labels = FALSE)
   # Razbijemo vektor na k seznamov na osnovi razreza intervalov
-  razbitje = split(r, razrez)
+  razbitje <- split(r, razrez)
   # zdaj imamo dane indekse za vsakega od k-tih delov
   
-  pp.napovedi = rep(0, nrow(podatki))
+  pp.napovedi <- rep(0, nrow(podatki_vsi))
   # prečno preverjanje
   for (i in 1:length(razbitje)){
     # učni podatki
@@ -100,10 +104,11 @@ napaka.cv <- function(podatki, formula, k){
     test.data <- data %>% filter(contains == TRUE) %>% select(-contains)
     
     # naučimo model
-    #model = lm(data = train.data, formula = formula)
+    mod.L <- lm(data = train.data, formula = formula)
+    mod.lmer <- lmer(data = trian.data, formula = formula)
     # napovemo za testne podatke
-    #napovedi = predict(model, newdata = test.data)
-    #pp.napovedi[ razbitje[[i]] ] = napovedi
+    napovedi <- predict(model, newdata = test.data)
+    pp.napovedi[ razbitje[[i]] ] <- napovedi
   }
 # izračunamo MSE
 napaka = mean((pp.napovedi - podatki$Prob) ^ 2)
@@ -117,6 +122,68 @@ for (i in 1:length(razbitje)){
   test.data = podatki.TTY[ razbitje[[i]], ]
 }
 
+
+#funkcija deli točne napovedi z vsoto vseh napovedi = točnost napovedi
+accuracy <- function(x)
+{sum(diag(x)/(sum(rowSums(x)))) * 100
+}
+
+
+# RANDOM FOREST
+library(randomForest)
+
+TTY.RF <- randomForest(TTY ~ ., data = train.data, mtry = 3,
+                         importance = TRUE, na.action = na.omit)
+TTY.RF
+plot(TTY.RF)
+
+# Predicting the Test set results
+y_pred = predict(TTY.RF, newdata = test.data[,-4])
+
+
+confusion_mtx = table(test.data$TTY, y_pred)
+
+accuracy(confusion_mtx2)
+
+napaka = mean((y_pred2 - test.data$TTY) ^ 2)
+napaka
+
+df.napak <- data.frame(cbind(test.data$TTY, round(y_pred2,4)))
+df.napak <- rename(df.napak, c("podatek" = "X1", "napoved"="X2"))
+df.napak <- df.napak %>% summarise(podatek, napoved,
+                                   abs_napaka = as.numeric(format(round(abs(y_pred2 - test.data$TTY),4), scientific=FALSE)),
+                                   kv_napaka = as.numeric(format(round((y_pred2 - test.data$TTY) ^ 2,4), scientific=FALSE)))
+colMeans(df.napak)
+
+
+
+
+
+
+# 2. PRISTOP - znotraj enega train.seta max 1 posel na id
+#========================================================
+
+k <- 10
+r2 <- unique(sample(podatki.TTY$ID))
+# razrežemo na k intervalov
+razrez <- cut(seq_along(r2), k, labels = FALSE)
+# Razbijemo vektor na k seznamov na osnovi razreza intervalov
+razbitje = split(r2, razrez)
+# zdaj imamo dane indekse za vsakega od k-tih delov
+
+# prečno preverjanje
+#FOR LOOP:  for (i in 1:length(razbitje)){
+# učni podatki
+data2 <- podatki.TTY %>% mutate(contains = ID %in% razbitje[[1]])
+train.data.2 <- data %>% group_by(ID) %>% sample_n(1) %>% 
+              filter(contains == FALSE) %>% select(-contains)
+# testni podatki
+test.data.2 <- data %>% group_by(ID) %>% sample_n(1) %>%
+              filter(contains == TRUE) %>% select(-contains)
+
+test.data.2$ID %in% train.data.2$ID
+
+length(unique(train.data.2$ID)) == 1038 - length(unique(test.data.2$ID))
 
 
 
